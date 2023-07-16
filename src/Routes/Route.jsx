4e6d7f80e0bcfa6730c3b6de0c387af3a13da855
shaikat17 <shaikatpal56@gmail.com>
2023-07-16 14:03:27 +0600

    Navbar component created.

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,18 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/login/Login";
 import Signup from "../pages/signup/Signup";
 import Home from "../pages/Home/Home";
+import Layout from "../Layout/Layout";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/signup",
-      element: <Signup />,
-    },
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/signup",
+          element: <Signup />,
+        },
+      ]
+    }
   ]);
