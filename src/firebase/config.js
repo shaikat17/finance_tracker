120705695b7a71f598f8accbaa2c0607e15554ec
shaikat17 <shaikatpal56@gmail.com>
2023-07-26 00:50:49 +0600

    Firebase and Firestore added.

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
