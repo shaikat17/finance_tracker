677e059756875a896f6cfd7b2d5012055565be93
shaikat17 <shaikatpal56@gmail.com>
2023-07-28 23:02:09 +0600

    Sign up page added.

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,11 +1,46 @@
+import { useState } from 'react'
+
+// styles
 import styles from './Signup.module.css'
 
-const Signup = () => {
-    return (
-        <div>
-            <h1>Sign Up</h1>
-        </div>
-    );
-};
+export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
 
-export default Signup;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, displayName)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input 
+          type="text" 
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  )
+}
