ae5957ce7624569155df5ed98e08277700b55c58
shaikat17 <shaikatpal56@gmail.com>
2023-07-28 23:21:00 +0600

    Sign up hook added.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 
-export { projectFirestore }
+export { projectFirestore, projectAuth }
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -17,7 +17,7 @@ export default function Signup() {
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
       <h2>Sign up</h2>
       <label>
-        <span>email:</span>
+        <span>Email:</span>
         <input 
           type="email" 
           onChange={(e) => setEmail(e.target.value)} 
@@ -25,7 +25,7 @@ export default function Signup() {
         />
       </label>
       <label>
-        <span>password:</span>
+        <span>Password:</span>
         <input 
           type="password" 
           onChange={(e) => setPassword(e.target.value)} 
@@ -33,7 +33,7 @@ export default function Signup() {
         />
       </label>
       <label>
-        <span>display name:</span>
+        <span>Display Name:</span>
         <input 
           type="text" 
           onChange={(e) => setDisplayName(e.target.value)}
