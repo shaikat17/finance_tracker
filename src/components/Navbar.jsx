60ae8c8f77a63440d3bd9c5f92757ac18f96f5a2
shaikat17 <shaikatpal56@gmail.com>
2023-07-30 00:19:12 +0600

    Logout functionality working.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css'
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import { useLogOut } from "../hooks/useLogout";
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbar}>
-            <ul>
-                <li className={styles.title}><Link to="/">My Money</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-            </ul>
-        </nav>
-    );
+  const { logOut } = useLogOut();
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>
+          <Link to="/">My Money</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+            <button className="btn" onClick={logOut}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
