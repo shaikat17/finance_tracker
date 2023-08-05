2e9170f676b8e29e1cabe89a22655d36e5de7fb8
shaikat17 <shaikatpal56@gmail.com>
2023-08-05 12:07:05 +0600

    Dynamic Navbar Functionality added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { useLogOut } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logOut } = useLogOut();
+
+  const { user } = useAuthContext()
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>
           <Link to="/">My Money</Link>
         </li>
+
+        {!user && <>
         <li>
           <Link to="/login">Login</Link>
         </li>
         <li>
           <Link to="/signup">Signup</Link>
         </li>
+        </>}
+
+        {user && 
+        <>
+        <li>Hello, {user?.displayName}</li>
         <li>
             <button className="btn" onClick={logOut}>Logout</button>
         </li>
+        </>
+        }
       </ul>
     </nav>
   );
