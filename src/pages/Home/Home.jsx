5b746189dd7c51d6086a1abe3f37e1700ec3ab59
shaikat17 <shaikatpal56@gmail.com>
2023-08-06 15:01:38 +0600

    Data showing successfully on the home page.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import styles from './Home.module.css'
 // components
 import TransactionForm from './TransactionForm'
 import { useCollection } from '../../hooks/useCollection'
+import TransactionList from './TransactionList'
 
 export default function Home() {
   const { user } = useAuthContext()
@@ -12,7 +13,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        Transaction List
+      {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
       </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid}/>
